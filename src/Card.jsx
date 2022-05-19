0e964a7964c478991a2485a6a261a7565c37592d
AlejandrObarrachina <alejandrobrokenh@gmail.com>
2022-05-19 10:53:27 +0200

    refactorización

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import "./App.scss";
 import CardContent from "./CardContent"
-import logof1 from "./assets/f1-logo-short.png";
 import ToogleButton from "./ToggleButton";
+import FrontCard from "./FrontCard"
+import logof1 from "./assets/f1-logo-short.png";
+
 
 const Card = (props) => {
     
     return (
         <div className="card-wrapper">
-            <div id="base" className="base">
-                <p>RACE: {props.time}</p>
-                <p>{props.day}</p>
-                <div className="title">
-                    <img className="logo" src={logof1} alt="logo f1" />
-                    <h2>{props.track}</h2>
-                </div>
-            </div>
-            <CardContent />
-            <ToogleButton/>
+            <FrontCard />
+            <CardContent  />
+            <ToogleButton />
         </div>
     );
    
--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -1,28 +1,16 @@
 import React from "react";
 import "./App.scss";
-import ReactDOM from 'react-dom';
 
 
 
-class ToggleButton extends React.compoment{
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {isOff: false};
-      }
-    handleClick() {
-        this.setState({isOff: !this.state.isOff})
-    }
+const ToggleButton = () => {
     
-    render () {
-        const { isOff } = this.state;
-        let title = this.state.isOff? "ON":"OFF";
-       
-             <button onClick={this.handleClick}>more {{ title }} </button>
+    return (
         
-    ;
-    }
-       
+            <button>more</button>
+           
+        
+    );
    
 };
 
