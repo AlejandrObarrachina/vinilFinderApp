6ed684a1ae3b2f992ad9dbaf0eb0342efdf0dd7c
AlejandrObarrachina <alejandrobrokenh@gmail.com>
2022-06-28 09:44:57 +0200

    adding data to db

--- src/Carrousel.jsx
+++ src/Carrousel.jsx
@@ -5,9 +5,9 @@ import CarrouselItem from "./CarrouselItem";
 const Carrousel = (props) => {
     
     const vinilData = [
-        {vinilName: "S16", content: "Alternative, Mechanical", author: "Woodkid", image: "https://www.mondosonoro.com/wp-content/uploads/2020/10/Woodkid-S16-disco.jpg"},
-        {vinilName: "Hurry Up!", content: "Alternative, BSO", author: "M83", image: "https://m.media-amazon.com/images/I/81tEvdYVSPL._AC_SL1417_.jpg"},
-        {vinilName: "Interstellar ", content: "Epic, BSO", author: "Hans Zimmer", image: "https://m.media-amazon.com/images/I/A1smtRIAUvL._SL1500_.jpg"}
+        {vinilName: "S16", content: "Alternative, Mechanical", author: "Woodkid", image: "https://www.mondosonoro.com/wp-content/uploads/2020/10/Woodkid-S16-disco.jpg", genero: "pop-español"},
+        {vinilName: "Hurry Up!", content: "Alternative, BSO", author: "M83", image: "https://m.media-amazon.com/images/I/81tEvdYVSPL._AC_SL1417_.jpg", genero: "pop-american"},
+        {vinilName: "Interstellar ", content: "Epic, BSO", author: "Hans Zimmer", image: "https://m.media-amazon.com/images/I/A1smtRIAUvL._SL1500_.jpg", genero: "rock"}
     ]
     return (
         
